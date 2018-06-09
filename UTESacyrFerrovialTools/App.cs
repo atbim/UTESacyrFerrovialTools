fae89954727f89ce87d7b63b47be90228d2f12ae
Adrian Used <adusvi@gmail.com>
2018-06-09 22:38:48 +0200

    Inicio UTE Sacyr Ferrovial Tools

--- UTESacyrFerrovialTools/App.cs
+++ UTESacyrFerrovialTools/App.cs
@@ -5,6 +5,7 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System.Reflection;
 #endregion
 
 namespace UTESacyrFerrovialTools
@@ -13,6 +14,18 @@ namespace UTESacyrFerrovialTools
     {
         public Result OnStartup(UIControlledApplication a)
         {
+            var thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            var tabName1 = "UTE Sacyr Ferrovial";
+            a.CreateRibbonTab(tabName1);
+
+            var panel1 = a.CreateRibbonPanel(tabName1, "Modelado Túnel");
+
+            // Creamos los botones del panel.
+            var button1 = panel1.AddItem(new PushButtonData("Button1", "Pantalla Pilotes", thisAssemblyPath, "UTESacyrFerrovialTools.InsertPiles")) as PushButton;
+            //button1.ToolTip = "Crea una sección por cada uno de los conjuntos existentes en el documento.";
+            //button1.LargeImage = new BitmapImage(new Uri(thisAssemblyPath.Remove(thisAssemblyPath.Length - 17) + @"\Resources\new_Profile.ico"));
+
             return Result.Succeeded;
         }
 
---
+++ UTESacyrFerrovialTools/InsertPiles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace UTESacyrFerrovialTools
+{
+    [Transaction(TransactionMode.Manual)]
+    public class InsertPiles : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            TaskDialog.Show("Dev", "Hello World!!!");
+
+            return Result.Succeeded;
+        }
+    }
+}
